ebd04fcd51fce9939f5fac979bba2c706886597a
Erwan Le Moigne <elmstudio@MacBook-Pro-de-Erwan.local>
2017-08-29 15:33:42 +0200

    Premier dépôt

---
+++ lang/paquet-dist_theme_penscratch_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    'dist_theme_penscratch_nom'         => "Thème Dist Penscratch",
+    'dist_theme_penscratch_slogan'      => "Un thème simple et épuré pour les personnes qui aiment écrire.",
+    'dist_theme_penscratch_description' => ""
+);
+
+?>
